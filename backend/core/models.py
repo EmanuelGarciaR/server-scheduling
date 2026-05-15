3c0e1ae8a22b9d3f391fea04c6c038df27843125
Emanuel Garcia Rios <135269247+EmanuelGarciaR@users.noreply.github.com>
2026-05-15 14:52:06 +0000

    [FEAT] Added remove_task in Server class

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,9 +1,11 @@
 import uuid
 
 class Task:
-    def __init__(self, task_name, task_time):
+    def __init__(self, task_name, task_time, task_priority=None):
         self.task_name = task_name
         self.task_time = task_time
+        self.priority = task_priority
+        self.dependency = []
         self.__id = self.__create_id()
 
     def __str__(self):
@@ -11,6 +13,9 @@ class Task:
 
     def __create_id(self) -> str:
         return str(uuid.uuid4())
+
+    def set_priority(self, priority):
+        self.priority = priority
 
     def get_id(self)-> str:
         return self.__id
@@ -26,10 +31,11 @@ class Task:
         return f"Task(name='{self.task_name}', time={self.task_time})"
 
 class Server:
-    def __init__(self, server_name: str):
+    def __init__(self, server_name: str, server_capacity: int):
         self.__server_id = self.create_server_id()
         self.server_name = server_name
         self.tasks = []
+        self.capacity = server_capacity
         self.total_load = 0
 
     def __create_server_id(self) -> str:
@@ -40,8 +46,19 @@ class Server:
 
     def add_task(self, task: Task):
         """Asigna una tarea al servidor y actualiza la carga total."""
+        #Validation capacity
+        if self.capacity is not None and self.total_load + task.task_time > self.capacity:
+            raise ValueError(f"Cannot add task '{task.task_name}' to server '{self.server_name}': capacity exceeded.")
         self.tasks.append(task)
         self.total_load += task.task_time
+
+    def remove_task(self, task: Task):
+        """Elimina una tarea del servidor y actualiza la carga total."""
+        if task in self.tasks:
+            self.tasks.remove(task)
+            self.total_load -= task.task_time
+        else:
+            raise ValueError(f"Task '{task.task_name}' not found in server '{self.server_name}'.")
 
     def clear(self):
         """Limpia las tareas asignadas."""
@@ -57,5 +74,5 @@ class Server:
         }
 
     def __repr__(self):
-        return f"Server(id='{self.get_server_id()}', load={self.total_load}, tasks={len(self.tasks)})"
+        return f"Server(id='{self.get_server_id()}', load={self.total_load}, tasks={len(self.tasks)}, capacity={self.capacity})"
 
